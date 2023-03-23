0436e0ed23a171f51cc53c4307a7a9845ee502d9
Kds010 <rkfdktkeo2@naver.com>
2023-03-23 17:01:44 +0900

    Initial commit

---
+++ src/main/java/kds/login_partten/LoginParttenApplication.java
@@ -0,0 +1,13 @@
+package kds.login_partten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginParttenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginParttenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kds/login_partten/LoginParttenApplicationTests.java
@@ -0,0 +1,13 @@
+package kds.login_partten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginParttenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
